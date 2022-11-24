3e610c40e40b8811115f21aec078c085c02f5286
rodraska <rodrigoreis1999@gmail.com>
2022-11-24 05:30:24 +0000

    compy

---
+++ Mealy.c
@@ -0,0 +1,62 @@
+#include "comp.h"
+
+char    *Mealy(char *str)
+{
+    int     i;
+    char    state;
+    char    *output;
+    
+    if(check_string(str) == 0)
+    {
+        printf("Invalid String\n");
+        return (NULL);
+    }
+    i = strlen(str) - 1;
+    state = 'A';
+    output = (char *)malloc(sizeof(char) * (strlen(str) + 1));
+    if(!output)
+        return (NULL);
+    output[strlen(str)] = '\0';
+    output += strlen(str) - 1;
+    str += strlen(str) - 1;
+    while (*str)
+    {
+        if (state == 'A')
+        {
+            if (*str == '0')
+            {
+                output[i] = '0';
+                state = 'A';
+            } 
+            else if (*str == '1')
+            {
+                output[i] = '1';
+                state = 'B';
+            }
+            i--;
+            str--;
+        }
+        else if (state == 'B')
+        {
+            if (*str == '0')
+            {
+                output[i] = '1';
+                state = 'B';
+            }
+            else if (*str == '1')
+            {
+                output[i] = '0';
+                state = 'B';
+            }
+            i--;
+            str--;
+        }
+    }
+    return (output);
+}
+
+int main(void)
+{
+    printf("%s\n", Mealy("1111"));
+    return (0);
+}
--- comp.h
+++ comp.h
@@ -3,11 +3,13 @@
 
 # include <stdio.h>
 # include <string.h>
+# include <stdlib.h>
 
 void    DFA(char *str);
 void    print_results_DFA(char state);
 int     check_string(char *str);
 void    NFA(char *str);
 void    print_results_NFA(char *state);
+char    *Mealy(char *str);
 
 #endif
